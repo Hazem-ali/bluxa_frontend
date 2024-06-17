e4e41ee5d6d5645e96ad2015fddc7c1f6251b3b0
Hazem Ali <hazemali627@gmail.com>
2024-06-17 03:40:22 +0300

    finishing navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "./App.css";
 import Navbar from "./common/navbar/navbar";
 import MainRoutes from "./routes/mainRoutes";
-import Backdrop from "./common/backdrop";
-import { useState } from "react";
+// import Backdrop from "./common/backdrop";
+// import { useState } from "react";
 
 function App() {
-  let [backdropShow, setBackdropShow] = useState(true);
+  // let [backdropShow, setBackdropShow] = useState(true);
   return (
-    <div className="bg-neutral-400">
-      <Navbar></Navbar>
-      <div className="flex flex-col gap-4 min-h-screen justify-center items-center">
-        <MainRoutes />
+    <div>
+      <Navbar />
+
+      <div className="bg-neutral-400">
+        <div className="flex flex-col gap-4 min-h-screen justify-center items-center">
+          <MainRoutes />
+        </div>
       </div>
-      <Backdrop
-        show={backdropShow}
-        onClick={() => setBackdropShow(!backdropShow)}
-      />
     </div>
   );
 }
--- src/common/navbar/navbar.jsx
+++ src/common/navbar/navbar.jsx
@@ -2,22 +2,27 @@ import React, { useState } from "react";
 import cd_image from "../../images/cd.png";
 import "./navbar.css";
 import "../../styles/global/button.css";
+import { Link, useNavigate } from "react-router-dom";
+import Backdrop from "../backdrop";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  const navigate = useNavigate();
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    console.log(isMenuOpen);
   };
 
   return (
-    <div className="">
-      <header className="flex w-full fixed justify-between items-center py-2 bg-blue-600 z-50">
-        <div className="flex ">
+    <header className="">
+      <Backdrop show={isMenuOpen} onClick={toggleMenu} />
+      <nav className="flex w-full h-16 fixed justify-between items-center py-2 px-6 bg-[#102C57] shadow-xl z-50">
+        <div className="flex items-center justify-center">
           {/* hamburger menu */}
-          <div className="sm:hidden">
-            <span className="hamburger-menu mx-2" onClick={toggleMenu}>
+          <div className="md:hidden">
+            <span
+              className="flex flex-col justify-around mx-2 w-7 h-7 cursor-pointer"
+              onClick={toggleMenu}
+            >
               <div className="bar"></div>
               <div className="bar"></div>
               <div className="bar"></div>
@@ -25,44 +30,50 @@ const Navbar = () => {
           </div>
 
           {/* icon */}
-          <div className="icon-div">
+          <div>
             <a href="/login" className="w-7 inline">
-              <img src={cd_image} alt="icon" />
+              <img className="w-8 mx-2" src={cd_image} alt="icon" />
             </a>
           </div>
         </div>
 
-        <nav className={` ${isMenuOpen ? "active" : ""}`}>
-          <ul className="text-white from-neutral-100">
-            <li className="main-nav__item sm:inline-block">
-              <a href="packages/index.html">Packages</a>
+        <div
+          className={`bg-[#102C57] md:static absolute md:shadow-none shadow-xl md:h-fit h-[40vh] duration-300 md:w-fit w-full flex items-center justify-center z-10 left-0 ${
+            isMenuOpen ? "top-[100%]" : "top-[-60vh]"
+          }`}
+        >
+          <ul className="text-white flex md:flex-row flex-col items-center md:gap-9 gap-8 ">
+            <li
+              className="hover:text-gray-400"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <Link to="/register">Packages</Link>
             </li>
-            <li className="main-nav__item sm:inline-block">
-              <a href="customers/index.html">Customers</a>
+            <li
+              className="hover:text-gray-400"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <Link to="/register">Customers</Link>
             </li>
-            <li className="main-nav__item sm:inline-block">
-              <a href="start-hosting/index.html">Start Hosting</a>
-            </li>
-          </ul>
-        </nav>
-
-        {/* mobile nav */}
-        <nav className="sm:hidden ">
-
-          <ul className=" top-0 left-0 fixed bg-white w-3/4 h-full">
-            <li className="main-nav__item">
-              <a href="packages/index.html">Packages</a>
-            </li>
-            <li className="main-nav__item">
-              <a href="customers/index.html">Customers</a>
-            </li>
-            <li className="main-nav__item">
-              <a href="start-hosting/index.html">Start Hosting</a>
+            <li
+              className="hover:text-gray-400"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <Link to="/register">Start Hosting</Link>
             </li>
           </ul>
-        </nav>
-      </header>
-    </div>
+        </div>
+        <button
+          className="text-black btn bg-comp"
+          onClick={() => {
+            setIsMenuOpen(false);
+            navigate("/login");
+          }}
+        >
+          Sign in
+        </button>
+      </nav>
+    </header>
   );
 };
 
